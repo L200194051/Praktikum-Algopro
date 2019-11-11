6eabc692b266aff91951633986d03adaebe46c56
L200194051 <55549320+L200194051@users.noreply.github.com>
2019-11-11 22:01:44 +0700

    Add files via upload

---
+++ praktikum 7 dafa/kegiatan2.py
@@ -0,0 +1,14 @@
+x = input('masukkan password: ')
+if x == 'Dafa':
+    print('Correct')
+else:
+    x = input('wrong,please input again: ')
+    if x == 'Dafa':
+        print('Correct')
+    else:
+        x = input('wrong,please input again: ')
+        if x == 'Dafa':
+            print('Correct')
+        else:
+            print('Acces has been blocked')
+        
